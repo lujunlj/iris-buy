5b374dcf86e732c0a763278c3208f20252c1fb91
lujunlj <434998516@qq.com>
2020-04-07 21:59:31 +0800

    2020年4月7日21:58:55 第二次提交 数据库查出数据转换到map[string]string

--- src/main/buy/common/mysql.go
+++ src/main/buy/common/mysql.go
@@ -7,7 +7,7 @@ import (
 
 //创建mysql 连接
 func NewMysqlConn() (db *sql.DB, err error) {
-	db, err = sql.Open("mysql", "root:imooc@tcp(127.0.0.1:3306)/imooc?charset=utf8")
+	db, err = sql.Open("mysql", "root:123456@tcp(192.168.11.199:3306)/imooc?charset=utf8")
 	return
 }
 
--- src/main/buy/dao/product_dao.go
+++ src/main/buy/dao/product_dao.go
@@ -3,7 +3,9 @@ package dao
 import (
 	"database/sql"
 	"errors"
+	"iris/src/main/buy/common"
 	"iris/src/main/buy/model"
+	"strconv"
 )
 
 //第一步.开发接口
@@ -33,8 +35,103 @@ func NewProductManager(table string, db *sql.DB) IProduct {
 	}
 }
 
-func (p *ProductManager) Conn() error {
-	return errors.New("123")
+//数据库连接
+func (p *ProductManager) Conn() (err error) {
+	if p.mysqlConn == nil {
+		conn, err := common.NewMysqlConn()
+		if err != nil {
+			return err
+		}
+		p.mysqlConn = conn
+	}
+	if p.table == "" {
+		p.table = "product"
+	}
+	return
 }
 
-//3.
+//插入
+func (p *ProductManager) Insert(pd *model.Product) (productId int64, err error) {
+
+	//判断连接是否存在
+	//准备sql
+	//传入参数
+	//执行sql获得id
+	if err = p.Conn(); err != nil {
+		return
+	}
+	sql := "Insert into product set productName = ? ,productNum=?,productImage=?,productUrl=?"
+	stmt, err := p.mysqlConn.Prepare(sql)
+	if err != nil {
+		return 0, err
+	}
+	result, err := stmt.Exec(pd.ProductName, pd.ProductNum, pd.ProductImage, pd.ProductUrl)
+	if err != nil {
+		return 0, err
+	}
+	productId, err = result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return productId, nil
+}
+
+//删除
+func (p *ProductManager) Delete(productID int64) bool {
+	//1.判断连接是否存在
+	if err := p.Conn(); err != nil {
+		return false
+	}
+	//2.准备sql
+	sql := "delete from product where id =?"
+	//3.根据sql 创建statement 填入参数
+	stmt, err := p.mysqlConn.Prepare(sql)
+	defer stmt.Close()
+	if err != nil {
+		return false
+	}
+	_, err = stmt.Exec(productID)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+//商品的更新
+func (p *ProductManager) Update(product *model.Product) (err error) {
+	//1.判断连接是否存在
+	if err := p.Conn(); err != nil {
+		return err
+	}
+	//2.准备sql
+	sql := "update product set productName =?,productNum=?,productImage=?,productUrl=? where id =" + strconv.FormatInt(product.ID, 10)
+	//3.根据sql 创建statement 填入参数
+	stmt, err := p.mysqlConn.Prepare(sql)
+	defer stmt.Close()
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(product.ProductName, product.ProductNum, product.ProductImage, product.ProductUrl)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *ProductManager) SelectByKey(productID int64) (product *model.Product, err error) {
+	//1.准备连接 判断连接是否存在
+	if err = p.Conn(); err != nil {
+		return &model.Product{}, err
+	}
+	//2.准备sql
+	sql := " select * from " + p.table + " where id =" + strconv.FormatInt(productID, 10)
+	row, errRow := p.mysqlConn.Query(sql)
+	if errRow != nil {
+		return &model.Product{}, errRow
+	}
+	result := common.GetResultRow(row)
+	if len(result) == 0 {
+		return &model.Product{}, nil
+	}
+
+}
